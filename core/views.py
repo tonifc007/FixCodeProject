0c07c94b9f85c6b79419d32da6cff37651798764
diegofsousa <diegofernando5672@gmail.com>
2016-11-15 22:59:13 -0300

    'Profile' 73%

--- core/views.py
+++ core/views.py
@@ -793,26 +793,49 @@ def un_favorite_fix(request, pk):
 
 
 def favorites(request, username):
-	if not request.user.is_authenticated():
-		return render(request, 'core/login.html')
+	use = get_object_or_404(User, username=username)
+	participations = Participations.objects.filter(user=use)
+	favorites = Favorites.objects.filter(user=use)
+	profile = get_object_or_404(Profile, user=use)
+	
+	instanciaSeguidor = Followers()
+
+	if request.user.is_authenticated():		
+		dadosSeguir = instanciaSeguidor.get_dados_seguidor(request.user, use)
+		dadosSDV = instanciaSeguidor.get_dados_seguidor(use, request.user)
+		eu = get_object_or_404(Profile, user=request.user)
+		data_comecou_seguir = instanciaSeguidor.get_data_que_comecou_seguir(request.user, use)
 	else:
-		user = get_object_or_404(User, username=username)
-		myfavorites = Favorites.objects.filter(user=user)
-		paginator = Paginator(myfavorites, 5)
+		dadosSeguir = dadosSDV = 0
+		eu = data_comecou_seguir = None
 
-		if user == request.user:
-			chave = True
-		else:
-			chave = False
+	followings = instanciaSeguidor.relacao_de_seguindo_decrescente(use)
+	followers = instanciaSeguidor.relacao_de_seguidores_decrescente(use)
 
-		page = request.GET.get('page')
-		try:
-			pagina = paginator.page(page)
-		except PageNotAnInteger:
-			pagina = paginator.page(1)
-		except EmptyPage:
-			pagina = paginator.page(paginator.num_pages)
-		return render(request, 'core/favorites.html', {'pagina':pagina, 'chave': chave, 'user': user})
+	paginator = Paginator(favorites[::-1], 5)
+	page = request.GET.get('page')
+	print(page)
+	print("Estou requisitando a {} página" .format(page))
+
+	try:
+		relations = paginator.page(page)
+		print(relations)
+	except PageNotAnInteger:
+		relations = paginator.page(1)
+	except EmptyPage:
+		relations = paginator.page(paginator.num_pages)
+	return render(request, 'core/favorites.html', {
+		'relations':relations, 'profile':profile,
+		'participations':participations,
+		'favorites':favorites,
+		'dadosSeguir':dadosSeguir,
+		'dadosSDV':dadosSDV,
+		'eu':eu,
+		'data':data_comecou_seguir,
+		'numerofollowings':followings,
+		'numerofollowers':followers,
+		'nfollowings':followings[0:9],
+		'nfollowers':followers[0:9]})
 
 
 def followajax(request, username):
